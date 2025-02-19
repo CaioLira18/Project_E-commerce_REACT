5f2d93e9192d9b244825eb71c5d33e15b48aab8a
CaioLira18 <caiozao1212@gmail.com>
2025-02-19 08:35:54 -0300

    Nav-Bar Add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,30 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-
-    <div className="header">
-        <Link to="/">
-        <h1>E-Commerce</h1>
-        </Link>
-    </div>
+        <>
+            <header>
+                <a className="logo" href="/"><img src="images/logo.svg" alt="logo do e-commerce" /></a>
+                <nav>
+                    <ul className="nav__links">
+                        <li><a href="#">Produtos</a></li>
+                        <li><a href="#">Hardwares</a></li>
+                        <li><a href="#">Sobre</a></li>
+                    </ul>
+                </nav>
+                <a className="cta" href="#">Contato</a>
+                <p className="menu cta">Home</p>
+            </header>
+            
+            <div className="overlay">
+                <a className="close" href="#">&times;</a>
+                <div className="overlay__content">
+                    <a href="#">Services</a>
+                    <a href="#">Projects</a>
+                    <a href="#">About</a>
+                </div>
+            </div>
+        </>
     );
 };
 
-export default Header
+export default Header;
